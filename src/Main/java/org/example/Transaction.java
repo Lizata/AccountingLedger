05b7ab8d41b0410379316ab1c96ece0dbce72309
Lizata <evelynelily98@gmail.com>
2023-10-27 16:06:15 -0400

    Fixed Transaction class

--- src/Main/java/org/example/Transaction.java
+++ src/Main/java/org/example/Transaction.java
@@ -5,34 +5,36 @@ import java.time.LocalTime;
 
 public class Transaction {
 
- private LocalDate date;
+    private LocalDate date;
 
- private LocalTime time;
- private String description;
- private String vendor;
- private double amount;
+    private LocalTime time;
+    private String description;
+    private String vendor;
+    private double amount;
 
- public Transaction(LocalDate dateInput, LocalTime timeInput, String inputDescription, String inputVendor, double inputAmount) {
-  this.date = dateInput;
-  this.time = timeInput;
-  this.description = inputDescription;
-  this.vendor = inputVendor;
-  this.amount = inputAmount;
- }
- public void setTransactionDate(LocalDate setDate){this.date = setDate;}
+    public Transaction(LocalDate dateInput, LocalTime timeInput, String inputDescription, String inputVendor, double inputAmount) {
+        this.date = dateInput;
+        this.time = timeInput;
+        this.description = inputDescription;
+        this.vendor = inputVendor;
+        this.amount = inputAmount;
+    }
 
- public void setTransactionTime(String timeInput){this.time = LocalTime;}
- public void setDescription(String description){this.description = setDescription();
-  public void setVendor(String inputVendor)
+    public void setTransactionDate(LocalDate setDate) {
+        this.date = setDate;
+    }
 
+    public void setTransactionTime(LocalTime timeInput) {
+        this.time = timeInput;
+    }
 
-  }
+    public void setDescription(String description) {
+        this.description = description;
 
+    }
 
-
-
- }
+    public void setVendor(String inputVendor) {
+        this.vendor = inputVendor;
+    }
 }
-
-
 
